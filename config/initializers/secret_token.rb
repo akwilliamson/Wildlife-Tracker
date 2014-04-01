726bdd358b63713552390576b1237fbe197df0dd
Aaron Williamson & Josh Paul <student@epicodus.com>
2014-04-01 09:18:49 -0700

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Wild::Application.config.secret_key_base = 'e31411a3e1be165a081516240948ebb7ddd04f3d59ed381a6c4d49832d125f73c4dc7beaf2d2e1b79ba886b1023bfb7aedcf10b6f0f6ac1617798cb69a545acc'
